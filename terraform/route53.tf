3f85a5a04f5953119fca4bd654cc0de73e6b01eb
JKolios <jason.kolios@gmail.com>
2022-07-12 21:50:16 +0300

    Adds wildcard record for pistack subdomains

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -10,3 +10,11 @@ resource "aws_route53_record" "pistack_a_record" {
   ttl     = "3600"
   records = [var.pistack_ip]
 }
+
+resource "aws_route53_record" "pistack_wildcard_a_record" {
+  zone_id = aws_route53_zone.pistack_domain_route53_zone.zone_id
+  name    = format("*.pistack.%s", var.domain_name)
+  type    = "A"
+  ttl     = "3600"
+  records = [var.pistack_ip]
+}
