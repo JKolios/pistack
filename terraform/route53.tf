6ba850c8ee5ec5953fde8717ecc10b121415ee3f
Jason Kolios <jason.kolios@workist.com>
2023-04-01 18:25:38 +0300

    Revert "Updates AAAA records,  removes A records"
    This reverts commit fc22f17f0f41cbfcda9d204ab58b338890e24bad.

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,3 +2,19 @@ resource "aws_route53_zone" "pistack_domain_route53_zone" {
   name = var.domain_name
   tags = var.common_tags
 }
+
+resource "aws_route53_record" "pistack_a_record" {
+  zone_id = aws_route53_zone.pistack_domain_route53_zone.zone_id
+  name    = format("pistack.%s", var.domain_name)
+  type    = "A"
+  ttl     = "3600"
+  records = [var.pistack_ip]
+}
+
+resource "aws_route53_record" "pistack_wildcard_a_record" {
+  zone_id = aws_route53_zone.pistack_domain_route53_zone.zone_id
+  name    = format("*.pistack.%s", var.domain_name)
+  type    = "A"
+  ttl     = "3600"
+  records = [var.pistack_ip]
+}
