026b7de744beedde2126e572a8290bfa8ea49a2e
Jason Kolios <jason.kolios@workist.com>
2023-04-02 20:59:29 +0300

    Project cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Pistack
 
 Docker-compose application stack to run on my home server(s). Currently a Raspberry Pi 4.
+
+Deployed by a github action on every commit to `main`.
--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "JKolios" # TF Cloud organization name
-    workspaces {
-      name = "pistack" # TF Cloud workspace name
-    }
-  }
-}
